e762649314222f094dffdd3ce8225f0f3c8a11a3
Rafael Nuno de Freitas Pilré <rafel.nuno@gmail.com>
2020-11-06 14:25:23 +0000

    tutorial

---
+++ helloworld/src/main/java/pt/iade/helloworld/models/CurricularUnit.java
@@ -0,0 +1,38 @@
+package pt.iade.helloworld.models;
+
+public class CurricularUnit{
+    private String name;
+    private double grade;
+    private double ects;
+    private int semester;
+
+    public CurricularUnit(String name, double grade, double ects, int semester) {
+        this.name = name;
+        this.grade = grade;
+        this.ects = ects;
+        this.semester = semester;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public double getEcts() {
+        return ects;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+    public boolean isAccepted() {
+        return grade >= 9.5;
+    }
+}
+
